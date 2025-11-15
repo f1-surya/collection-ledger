c76aff22d0db8851f042944f7c773b4bbe7451d0
surya <balasurya811@gmail.com>
2025-11-15 19:18:35 +0530

    Payments export is more detailed.
    Instead of just sending the payments list the page now saves a zip file
    containing the migrations, payments and unpaid list CSVs.

--- src/app/api/payment/sheet/route.ts
+++ src/app/api/payment/sheet/route.ts
@@ -1,8 +1,9 @@
-import { endOfMonth, startOfMonth } from "date-fns";
+import { endOfMonth, format, startOfMonth, subMonths } from "date-fns";
 import { and, between, eq } from "drizzle-orm";
+import JSZip from "jszip";
 import type { NextRequest } from "next/server";
 import { db } from "@/db/drizzle";
-import { connections, payments } from "@/db/schema";
+import { basePacks, connections, payments } from "@/db/schema";
 import { getOrg } from "@/lib/get-org";
 import { writeCsv } from "@/lib/sheet";
 
@@ -12,23 +13,85 @@ export async function GET(req: NextRequest) {
     ? // biome-ignore lint/style/noNonNullAssertion: It is asserted at the top, just not with a variable
       new Date(params.get("month")!.toString())
     : new Date();
+  const start = startOfMonth(month);
+  const end = endOfMonth(month);
 
   const org = await getOrg();
-  const data = await db
-    .select({ boxNumber: connections.boxNumber })
-    .from(payments)
-    .where(
-      and(
-        eq(payments.org, org.id),
-        between(payments.date, startOfMonth(month), endOfMonth(month)),
-        eq(payments.isMigration, false),
+  const [currPayments, unpaidConnections] = await Promise.all([
+    db
+      .select({
+        boxNumber: connections.boxNumber,
+        isMigration: payments.isMigration,
+        to: basePacks.name,
+      })
+      .from(payments)
+      .where(and(eq(payments.org, org.id), between(payments.date, start, end)))
+      .rightJoin(connections, eq(payments.connection, connections.id))
+      .leftJoin(basePacks, eq(payments.to, basePacks.id)),
+    db
+      .select({ boxNumber: connections.boxNumber })
+      .from(connections)
+      .where(
+        and(
+          eq(connections.org, org.id),
+          between(
+            connections.lastPayment,
+            subMonths(start, 1),
+            subMonths(end, 1),
+          ),
+        ),
       ),
-    )
-    .rightJoin(connections, eq(payments.connection, connections.id));
+  ]);
 
-  const csv = writeCsv([["Vc Number"], ...data.map((p) => [p.boxNumber])]);
+  const zip = new JSZip();
 
-  return new Response(csv, {
-    headers: { "Content-Type": "text/csv" },
+  zip.file(
+    "Paid_SMCS.csv",
+    writeCsv([
+      ["Vc Number"],
+      ...currPayments
+        .filter((pay) => !pay.isMigration)
+        .map((p) => [p.boxNumber]),
+    ]),
+  );
+
+  const migrations = currPayments
+    .filter((pay) => pay.isMigration)
+    .reduce(
+      (acc, payment) => {
+        const packName = payment.to ?? "";
+        const smcs = acc[packName] ?? [];
+        acc[packName] = [...smcs, [payment.boxNumber]];
+        return acc;
+      },
+      {} as { [key: string]: string[][] },
+    );
+
+  if (Object.keys(migrations).length > 0) {
+    for (const pack in migrations) {
+      zip.file(
+        `MIGRATION_${pack.replace(" ", "_")}.csv`,
+        writeCsv([["Vc Number"], ...migrations[pack]]),
+      );
+    }
+  }
+
+  if (unpaidConnections.length > 0) {
+    zip.file(
+      "UNPAID_SMCS.csv",
+      writeCsv([
+        ["Vc Number"],
+        ...unpaidConnections.map((con) => [con.boxNumber]),
+      ]),
+    );
+  }
+
+  const content = await zip.generateAsync({ type: "blob" });
+
+  return new Response(content, {
+    headers: {
+      "Content-Type": "application/zip",
+      "Content-Disposition": `attachment; filename=${format(start, "MMM-yyyy")}.zip`,
+    },
   });
 }
